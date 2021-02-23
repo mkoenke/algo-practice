057748a12a05ba7411189aa1b05c6a532ce3b970
mkoenke <mkoenke@sas.upenn.edu>
2021-02-23 12:14:49 -0500

    add problem set for freq counter and multiple pointers

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -45,3 +45,38 @@ function validAnagram(string1, string2) {
   }
   return true
 }
+//exercise 3
+function sameFrequency(num1, num2) {
+  // good luck. Add any arguments you deem necessary.
+  let string1 = num1.toString()
+  let string2 = num2.toString()
+  if (string1.length !== string2.length) {
+    return false
+  }
+  let freqCount1 = {}
+  let freqCount2 = {}
+  for (let digit of string1) {
+    freqCount1[digit] = (freqCount1[digit] || 0) + 1
+  }
+  for (let digit of string2) {
+    freqCount2[digit] = (freqCount2[digit] || 0) + 1
+  }
+  for (let key in freqCount1) {
+    if (freqCount1[key] !== freqCount2[key]) {
+      return false
+    }
+  }
+  return true
+}
+//exercise 4 - frequency counter
+
+function areThereDuplicates() {
+  let collection = {}
+  for (let val in arguments) {
+    collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+  }
+  for (let key in collection) {
+    if (collection[key] > 1) return true
+  }
+  return false
+}
--- mulitplePointers.js
+++ mulitplePointers.js
@@ -12,3 +12,52 @@ function sumZero(array) {
     }
   }
 }
+
+//exercise 4
+function areThereDuplicates(...args) {
+  // Two pointers
+  args.sort((a, b) => a > b)
+  let start = 0
+  let next = 1
+  while (next < args.length) {
+    if (args[start] === args[next]) {
+      return true
+    }
+    start++
+    next++
+  }
+  return false
+}
+
+function areThereDuplicates() {
+  return new Set(arguments).size !== arguments.length
+}
+
+// exercise 5
+
+function averagePair(array, num) {
+  // add whatever parameters you deem necessary - good luck!
+  let left = 0
+  let right = array.length - 1
+  while (left < right) {
+    let avg = array[left] + array[right] / 2
+    if (avg === num) {
+      return true
+    } else if (avg < num) {
+      left++
+    } else right--
+  }
+  return false
+}
+
+function isSubsequence(str1, str2) {
+  var i = 0
+  var j = 0
+  if (!str1) return true
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++
+    if (i === str1.length) return true
+    j++
+  }
+  return false
+}
