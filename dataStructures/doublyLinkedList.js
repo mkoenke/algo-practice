c3615e6ab6a14f3d3ba1537896807b9fb7f50de9
mkoenke <mkoenke@sas.upenn.edu>
2021-02-25 17:26:38 -0500

    add DLL get, set, insert, remove

--- dataStructures/doublyLinkedList.js
+++ dataStructures/doublyLinkedList.js
@@ -66,4 +66,63 @@ class DoublyLinkedList {
     this.length++
     return this
   }
+  get(index) {
+    if (index < 0 || index >= this.length) return null
+    if (index <= this.length / 2) {
+      let count = 0
+      let current = this.head
+      while (count !== index) {
+        current = current.next
+        count++
+      }
+      return current
+    } else {
+      let count = this.length - 1
+      let current = this.tail
+      while (count !== index) {
+        current = current.prev
+        count--
+      }
+      return current
+    }
+  }
+  set(index, value) {
+    let foundNode = this.get(index)
+    if (!foundNode) {
+      return false
+    } else {
+      foundNode.val = value
+      return true
+    }
+  }
+  insert(index, value) {
+    if (index < 0 || index >= this.length) return false
+    if (index === 0) return !!this.unshift(value)
+    if (index === this.length) return !!this.push(value)
+    let newNode = newNode(value)
+    let beforeNode = this.get(index - 1)
+    let afterNode = beforeNode.next
+    beforeNode.next = newNode
+    newNode.prev = beforeNode
+    newNode.next = afterNode
+    afterNode.prev = newNode
+    this.length++
+    return true
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined
+    if (index === 0) return this.shift()
+    if (index === this.length - 1) return this.pop()
+    let removedNode = this.get(index)
+    // removedNode.prev.next = removedNode.next
+    // removedNode.next.prev = removedNode.prev
+    let beforeNode = removedNode.prev
+    let afterNode = removedNode.next
+    beforeNode.next = afterNode
+    afterNode.prev = beforeNode
+    removedNode.next = null
+    removedNode.prev = null
+    this.length--
+    return removedNode
+  }
 }
