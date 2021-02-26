98ea3e874f44566bb39c3ea5c620b2b7f90b8fa1
mkoenke <mkoenke@sas.upenn.edu>
2021-02-26 12:29:08 -0500

    add priority queue with min heap

--- dataStructures/priorityQueue.js
+++ dataStructures/priorityQueue.js
@@ -1,32 +1,42 @@
-class MaxBinaryHeap {
+class Node {
+  constructor(value, priority) {
+    this.value = value
+    this.priority = priority
+  }
+}
+class PriorityQueue {
   constructor() {
     this.values = []
   }
-  insert(element) {
-    this.values.push(element)
-    this.bubbleUp(element)
+  enqueue(value, priority) {
+    let newNode = new Node(value, priority)
+    this.values.push(newNode)
+    // this.bubbleUp(newNode)
+    this.bubbleUp()
   }
-  bubbleUp(element) {
+  //   bubbleUp(newNode) {
+  bubbleUp() {
     let index = this.values.length - 1
+    const newNode = this.values[index]
     while (index > 0) {
       let parentIndex = Math.floor((index - 1) / 2)
       let parent = this.values[parentIndex]
-      if (element <= parent) break
-      if (element > parent) {
-        this.values[parentIndex] = element
-        this.values[index] = parent
-        index = parentIndex
-      }
+      if (newNode.priority >= parent.priority) break
+      //   if (newNode.priority < parent.priority) {
+      this.values[parentIndex] = newNode
+      this.values[index] = parent
+      index = parentIndex
+      //   }
     }
   }
-  extractMax() {
-    const max = this.values[0]
+  dequeue() {
+    const min = this.values[0]
     const end = this.values.pop()
     if (this.values.length > 0) {
       this.values[0] = end
       this.sinkDown()
     }
-    return max
+    return min
   }
 
   sinkDown() {
@@ -40,15 +50,15 @@ class MaxBinaryHeap {
       let swapIndex = null
       if (leftChildIndex < length) {
         leftChild = this.values[leftChildIndex]
-        if (leftChild > element) {
+        if (leftChild.priority < element.priority) {
           swapIndex = leftChildIndex
         }
       }
       if (rightChildIndex < length) {
         rightChild = this.values[rightChildIndex]
         if (
-          (swapIndex === null && rightChild > element) ||
-          (swapIndex !== null && rightChild > leftChild)
+          (swapIndex === null && rightChild.priority < element.priority) ||
+          (swapIndex !== null && rightChild.priority < leftChild.priority)
         ) {
           swapIndex = rightChildIndex
         }
@@ -61,3 +71,10 @@ class MaxBinaryHeap {
     }
   }
 }
+//min heap with priority queue
+let ER = new PriorityQueue()
+ER.enqueue("common cold", 5)
+ER.enqueue("GSW", 1)
+ER.enqueue("fever", 4)
+ER.enqueue("broken arm", 2)
+ER.enqueue("gaping wound", 3)
