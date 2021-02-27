dcd4e8c67191657b81920069542ff67878c471f9
mkoenke <mkoenke@sas.upenn.edu>
2021-02-27 15:21:07 -0500

    add optimized priority queue with dijkstras

--- dataStructures/dijkstras.js
+++ dataStructures/dijkstras.js
@@ -59,19 +59,94 @@ class WeightedGraph {
   }
 }
 
+///slow bc not binary heap
+// class PriorityQueue {
+//   constructor() {
+//     this.values = []
+//   }
+//   enqueue(value, priority) {
+//     this.values.push({ value, priority })
+//     this.sort()
+//   }
+//   dequeue() {
+//     return this.values.shift()
+//   }
+//   sort() {
+//     this.values.sort((a, b) => a.priority - b.priority)
+//   }
+// }
+
 class PriorityQueue {
   constructor() {
     this.values = []
   }
   enqueue(value, priority) {
-    this.values.push({ value, priority })
-    this.sort()
+    let newNode = new Node(value, priority)
+    this.values.push(newNode)
+    // this.bubbleUp(newNode)
+    this.bubbleUp()
+  }
+  //   bubbleUp(newNode) {
+  bubbleUp() {
+    let index = this.values.length - 1
+    const newNode = this.values[index]
+    while (index > 0) {
+      let parentIndex = Math.floor((index - 1) / 2)
+      let parent = this.values[parentIndex]
+      if (newNode.priority >= parent.priority) break
+      //   if (newNode.priority < parent.priority) {
+      this.values[parentIndex] = newNode
+      this.values[index] = parent
+      index = parentIndex
+      //   }
+    }
   }
   dequeue() {
-    return this.values.shift()
+    const min = this.values[0]
+    const end = this.values.pop()
+    if (this.values.length > 0) {
+      this.values[0] = end
+      this.sinkDown()
+    }
+    return min
   }
-  sort() {
-    this.values.sort((a, b) => a.priority - b.priority)
+
+  sinkDown() {
+    let index = 0
+    const length = this.values.length
+    const element = this.values[0]
+    while (true) {
+      let leftChildIndex = 2 * index + 1
+      let rightChildIndex = 2 * index + 2
+      let leftChild, rightChild
+      let swapIndex = null
+      if (leftChildIndex < length) {
+        leftChild = this.values[leftChildIndex]
+        if (leftChild.priority < element.priority) {
+          swapIndex = leftChildIndex
+        }
+      }
+      if (rightChildIndex < length) {
+        rightChild = this.values[rightChildIndex]
+        if (
+          (swapIndex === null && rightChild.priority < element.priority) ||
+          (swapIndex !== null && rightChild.priority < leftChild.priority)
+        ) {
+          swapIndex = rightChildIndex
+        }
+      }
+
+      if (swapIndex === null) break
+      this.values[index] = this.values[swapIndex]
+      this.values[swapIndex] = element
+      index = swapIndex
+    }
+  }
+}
+class Node {
+  constructor(value, priority) {
+    this.value = value
+    this.priority = priority
   }
 }
 
