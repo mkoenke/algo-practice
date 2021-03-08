3ef3159fe17df2107c7b4b4b2eba1e197641a0bf
mkoenke <mkoenke@sas.upenn.edu>
2021-03-08 15:18:04 -0500

    add bubble count swaps

---
+++ dataStructures/problems/bubbleCountSwaps.js
@@ -0,0 +1,16 @@
+function countSwaps(a) {
+  let numSwaps = 0
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < a.length - 1; j++) {
+      if (a[j] > a[j + 1]) {
+        let temp = a[j]
+        a[j] = a[j + 1]
+        a[j + 1] = temp
+        numSwaps++
+      }
+    }
+  }
+  console.log(`Array is sorted in ${numSwaps} swaps.`)
+  console.log(`First Element: ${a[0]}`)
+  console.log(`Last Element: ${a[a.length - 1]}`)
+}
