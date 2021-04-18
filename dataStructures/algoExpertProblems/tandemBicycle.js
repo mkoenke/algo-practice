ce490d19028f07637b4d019f562f44ca549a8d47
mkoenke <mkoenke@sas.upenn.edu>
2021-04-18 16:37:26 -0400

    add tandem bicycle

---
+++ dataStructures/algoExpertProblems/tandemBicycle.js
@@ -0,0 +1,28 @@
+function tandemBicycle(redShirtSpeeds, blueShirtSpeeds, fastest) {
+  // Write your code here.
+  redShirtSpeeds.sort((a, b) => a - b)
+  blueShirtSpeeds.sort((a, b) => a - b)
+
+  if (!fastest) reverseSorting(redShirtSpeeds)
+
+  let totalSpeed = 0
+  for (let i = 0; i < redShirtSpeeds.length; i++) {
+    const first = redShirtSpeeds[i]
+    const second = blueShirtSpeeds[blueShirtSpeeds.length - 1 - i]
+    totalSpeed += Math.max(first, second)
+  }
+  return totalSpeed
+}
+
+function reverseSorting(array) {
+  let start = 0
+  let end = array.length - 1
+
+  while (start < end) {
+    const temp = array[start]
+    array[start] = array[end]
+    array[end] = temp
+    start++
+    end--
+  }
+}
