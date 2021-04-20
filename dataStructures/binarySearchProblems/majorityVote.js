7f31f7461594667123079467ffadb1bba3122554
mkoenke <mkoenke@sas.upenn.edu>
2021-04-20 12:09:48 -0400

    add O(1) space solution for majority vote

--- dataStructures/binarySearchProblems/majorityVote.js
+++ dataStructures/binarySearchProblems/majorityVote.js
@@ -11,3 +11,25 @@ function majorityVote(nums) {
   }
   return -1
 }
+
+function majorityVote(nums) {
+  nums.sort((a, b) => a - b)
+  const half = Math.floor(nums.length / 2)
+  let count = 0
+  let left = 0
+  let right = 0
+  if (nums.length === 1) return nums[0]
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[left] === nums[right]) {
+      count++
+      right++
+      if (count > half) return nums[i - 1]
+    } else {
+      if (count > half) return nums[i - 1]
+      left = i
+      count = 1
+      right++
+    }
+  }
+  return -1
+}
