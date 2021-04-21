5da08af9d91b1fa2ca6897da3d42f08a8050aefe
mkoenke <mkoenke@sas.upenn.edu>
2021-04-21 12:06:28 -0400

    add phone number mnemonics

---
+++ dataStructures/algoExpertProblems/phoneNumberMnemonics.js
@@ -0,0 +1,35 @@
+const digitLetterHash = {
+  0: ['0'],
+  1: ['1'],
+  2: ['a', 'b', 'c'],
+  3: ['d', 'e', 'f'],
+  4: ['g', 'h', 'i'],
+  5: ['j', 'k', 'l'],
+  6: ['m', 'n', 'o'],
+  7: ['p', 'q', 'r', 's'],
+  8: ['t', 'u', 'v'],
+  9: ['w', 'x', 'y', 'z'],
+}
+
+function phoneNumberMnemonics(phoneNumber) {
+  const currentCombo = new Array(phoneNumber.length).fill('0')
+  const allMnemonics = []
+  recursiveMnemonicFinder(0, phoneNumber, currentCombo, allMnemonics)
+  return allMnemonics
+}
+
+function recursiveMnemonicFinder(i, phoneNumber, currentCombo, allMnemonics) {
+  if (i === phoneNumber.length) {
+    const mnemonic = currentCombo.join('')
+    allMnemonics.push(mnemonic)
+  } else {
+    const digit = phoneNumber[i]
+    const lettersArray = digitLetterHash[digit]
+
+    for (let j = 0; j < lettersArray.length; j++) {
+      const letter = lettersArray[j]
+      currentCombo[i] = letter
+      recursiveMnemonicFinder(i + 1, phoneNumber, currentCombo, allMnemonics)
+    }
+  }
+}
--- dataStructures/algoExpertProblems/riverLengths.js
+++ dataStructures/algoExpertProblems/riverLengths.js
@@ -1,10 +1,10 @@
 function riverSizes(matrix) {
   // Write your code here.
   const riverSizes = []
-  const visitedMatrix = maptrix.map((row) => row.map((value) => false))
+  const visitedMatrix = matrix.map((row) => row.map((value) => false))
 
   for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
       if (visitedMatrix[i][j]) continue
       checkNeighbors(i, j, matrix, visitedMatrix, riverSizes)
     }
@@ -20,8 +20,35 @@ function checkNeighbors(i, j, matrix, visitedMatrix, riverSizes) {
     i = currentNode[0]
     j = currentNode[1]
     if (visitedMatrix[i][j]) continue
-    vistedMatrix[i][j] = true
+    visitedMatrix[i][j] = true
     if (matrix[i][j] === 0) continue
     currentRiverSize++
+    const unvisitedNeighbors = getUnvisited(i, j, matrix, visitedMatrix)
+    // for  (let i = 0; i < unvisitedNeighbors.length; i++){
+    // 	nodesToExplore.push(unvisitedNeighbors[i])
+    // }
+    for (const neighbor of unvisitedNeighbors) {
+      nodesToExplore.push(neighbor)
+    }
   }
+  if (currentRiverSize > 0) {
+    riverSizes.push(currentRiverSize)
+  }
+}
+
+function getUnvisited(i, j, matrix, visitedMatrix) {
+  const unvisitedNeighbors = []
+  if (i > 0 && !visitedMatrix[i - 1][j]) {
+    unvisitedNeighbors.push([i - 1][j])
+  }
+  if (i < matrix.length - 1 && !visitedMatrix[i + 1][j]) {
+    unvisitedNeighbors.push([i + 1][j])
+  }
+  if (j > 0 && !visitedMatrix[i][j - 1]) {
+    unvisitedNeighbors.push([i, j - 1])
+  }
+  if (j < matrix[0].length - 1 && !visitedMatrix[i][j + 1]) {
+    unvisitedNeighbors.push([i, j + 1])
+  }
+  return unvisitedNeighbors
 }
