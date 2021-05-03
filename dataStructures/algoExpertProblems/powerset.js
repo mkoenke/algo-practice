b4010f7e11e8330f7bbfaa29a623dd2373f17dc0
mkoenke <mkoenke@sas.upenn.edu>
2021-05-03 18:29:23 -0400

    add powerset

---
+++ dataStructures/algoExpertProblems/powerset.js
@@ -0,0 +1,17 @@
+function powerset(array, idx = null) {
+  // Write your code here.
+  if (idx === null) {
+    idx = array.length - 1
+  }
+  if (idx < 0) {
+    return [[]]
+  }
+  const element = array[idx]
+  const subsets = powerset(array, idx - 1)
+  const length = subsets.length
+  for (let i = 0; i < length; i++) {
+    const currentSubset = subsets[i]
+    subsets.push(currentSubset.concat(element))
+  }
+  return subsets
+}
